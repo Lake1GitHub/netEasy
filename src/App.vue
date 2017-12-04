0ce84352fd91dfcba4d7071160a32d28bf894ec6
Lake1GitHub <1695064014@qq.com>
2017-12-04 18:24:46 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <router-view/>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+<style>
+    *{ margin: 0; padding: 0;}
+    #app{ background-color: rgb(245, 245, 245); }
+</style>
